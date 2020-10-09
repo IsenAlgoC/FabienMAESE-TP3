0f9e32871af6ee636c8f65ec7af719a6203dfab6
Fabien MAESE <fabien.maese@isen.yncrea.fr>
2020-10-09 09:16:09 +0200

    Exercice 2

---
+++ Exo2/Exo2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	unsigned long int entier = 2868838400;
+	int tailleoctets = sizeof(entier);
+	int taillebits = 8 * tailleoctets;
+	int i = 0;
+	printf("On a besoin de %d octets et de %d bits pour coder 2868838400\n", tailleoctets, taillebits);
+
+	while (entier>0) {
+
+		if (entier & 1 == 1) {
+			printf("Bit %d :  ON\n", i);
+		}
+
+		else {
+			printf("Bit %d :  OFF\n", i);
+		}
+
+		entier = entier >> 1;
+		i=i+1;
+	}
+
+	// tous les bits sont bien affichés et cela correspond bien au nombre donné.
+
+	printf("\nBye ! Have a nice day !\n");
+
+}
